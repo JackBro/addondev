45f0770ef927070df214fd272e3012f60109e488
s <s@localhost>
2009-11-18 03:12:10 +0900

    -

--- AddonDev/src/jp/addondev/editor/javascript/JavaScriptEditor.java
+++ AddonDev/src/jp/addondev/editor/javascript/JavaScriptEditor.java
@@ -1,52 +1,193 @@
 package jp.addondev.editor.javascript;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jp.addondev.parser.javascript.JsNode;
 import jp.addondev.parser.javascript.Lexer;
 import jp.addondev.parser.javascript.NodeManager;
 import jp.addondev.parser.javascript.Parser;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioningListener;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.IVerticalRuler;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+import org.eclipse.wst.jsdt.core.IJavaScriptProject;
+import org.eclipse.wst.jsdt.core.JavaScriptCore;
 import org.eclipse.wst.jsdt.internal.ui.JavaScriptPlugin;
 import org.eclipse.wst.jsdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.wst.jsdt.internal.ui.javaeditor.EditorUtility;
+import org.eclipse.wst.jsdt.internal.ui.javaeditor.JavaSourceViewer;
+import org.eclipse.wst.jsdt.internal.ui.text.PreferencesAdapter;
 import org.eclipse.wst.jsdt.ui.text.IJavaScriptPartitions;
+import org.eclipse.wst.jsdt.ui.text.JavaScriptSourceViewerConfiguration;
 import org.eclipse.wst.jsdt.ui.text.JavaScriptTextTools;
 
 
-public class JavaScriptEditor extends TextEditor {
-	private JavaScriptOutlinePage outlinePage = null;
-	
+//public class JavaScriptEditor extends TextEditor {
+//	private JavaScriptOutlinePage outlinePage = null;
+//	
+//	public JavaScriptEditor() {
+//		super();
+//		// TODO Auto-generated constructor stub
+//		
+////        IPreferenceStore store= createCombinedPreferenceStore(null);
+////		setPreferenceStore(store);
+//		IPreferenceStore store = getPreferenceStore();
+//		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();		
+//		setSourceViewerConfiguration(new MyJavaScriptConfiguration(textTools.getColorManager(), 
+//				store, this, IDocument.DEFAULT_CONTENT_TYPE));
+//	}
+//
+//	@Override
+//	protected void initializeEditor() {
+//		// TODO Auto-generated method stub
+//		//super.initializeEditor();
+//		
+////        IPreferenceStore store= createCombinedPreferenceStore(null);
+////		//setPreferenceStore(store);
+////		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();		
+////		setSourceViewerConfiguration(new MyJavaScriptConfiguration(textTools.getColorManager(), 
+////				store, this, IDocument.DEFAULT_CONTENT_TYPE));
+//	}
+//
+//	@Override
+//	protected void setPreferenceStore(IPreferenceStore store) {
+//		// TODO Auto-generated method stub
+//		super.setPreferenceStore(store);
+//		
+////		if (getSourceViewerConfiguration() instanceof MyJavaScriptConfiguration) {
+////			JavaScriptTextTools textTools=JavaScriptPlugin.getDefault().getJavaTextTools();	
+////			setSourceViewerConfiguration(new MyJavaScriptConfiguration(textTools.getColorManager(), store, this, IDocument.DEFAULT_CONTENT_TYPE));
+////		}
+////		if (getSourceViewer() instanceof JavaSourceViewer)
+////			((JavaSourceViewer)getSourceViewer()).setPreferenceStore(store);
+//	}
+//
+////	private IPreferenceStore createCombinedPreferenceStore(IEditorInput input) {
+////		List stores= new ArrayList(3);
+////
+////		IJavaScriptProject project= EditorUtility.getJavaProject(input);
+////		if (project != null) {
+////			//stores.add(new EclipsePreferencesAdapter(new ProjectScope(project.getProject()), JavaScriptCore.PLUGIN_ID));
+////		}
+////
+////		stores.add(JavaScriptPlugin.getDefault().getPreferenceStore());
+////		stores.add(new PreferencesAdapter(JavaScriptCore.getPlugin().getPluginPreferences()));
+////		stores.add(EditorsUI.getPreferenceStore());
+////
+////		return new ChainedPreferenceStore((IPreferenceStore[]) stores.toArray(new IPreferenceStore[stores.size()]));
+////	}
+//	
+////	@Override
+////	protected ISourceViewer createSourceViewer(Composite parent,
+////			IVerticalRuler ruler, int styles) {
+////		// TODO Auto-generated method stub
+////		return new AdaptedSourceViewer(parent, verticalRuler, overviewRuler, isOverviewRulerVisible, styles, store);
+////		//return super.createSourceViewer(parent, ruler, styles);
+////	}
+//
+////	@Override
+////	public Object getAdapter(Class adapter) {
+////		//TODO Auto-generated method stub
+////		if (IContentOutlinePage.class.equals(adapter)) {
+////			if(outlinePage == null) outlinePage = new JavaScriptOutlinePage(this);
+////			return outlinePage;
+////			
+////			//return super.getAdapter(adapter);
+////		}
+////
+////		return super.getAdapter(adapter);
+////	}
+//	
+//}
+
+public class JavaScriptEditor extends CompilationUnitEditor
+{
 	public JavaScriptEditor() {
-		super();
 		// TODO Auto-generated constructor stub
-		
-		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();
-		setSourceViewerConfiguration(new MyJavaScriptConfiguration(textTools.getColorManager(), 
-				getPreferenceStore(), this, IJavaScriptPartitions.JAVA_PARTITIONING));
+		super();
+
 	}
 
-//	@Override
-//	public Object getAdapter(Class adapter) {
-//		//TODO Auto-generated method stub
-//		if (IContentOutlinePage.class.equals(adapter)) {
-//			if(outlinePage == null) outlinePage = new JavaScriptOutlinePage(this);
-//			return outlinePage;
-//			
-//			//return super.getAdapter(adapter);
-//		}
-//
-//		return super.getAdapter(adapter);
-//	}
+
+
+	@Override
+	protected void initializeEditor() {
+		// TODO Auto-generated method stub
+		super.initializeEditor();
+		IPreferenceStore store= createCombinedPreferenceStore(null);
+		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();
+		setSourceViewerConfiguration(
+				new MyJavaScriptConfiguration(textTools.getColorManager(), 
+						getPreferenceStore(), this, IDocument.DEFAULT_CONTENT_TYPE));	
+	}
+
+
+
+	@Override
+	protected JavaScriptSourceViewerConfiguration createJavaSourceViewerConfiguration() {
+		// TODO Auto-generated method stub
+		//return super.createJavaSourceViewerConfiguration();
+		
+		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();
+		
+		return new MyJavaScriptConfiguration(textTools.getColorManager(), 
+						getPreferenceStore(), this, IDocument.DEFAULT_CONTENT_TYPE);
+	}
 	
+	
+	
+	@Override
+	protected void setPreferenceStore(IPreferenceStore store) {
+		// TODO Auto-generated method stub
+//		super.setPreferenceStore(store);
+//		
+//		if (getSourceViewerConfiguration() instanceof JavaScriptSourceViewerConfiguration) {
+//			JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();
+//			setSourceViewerConfiguration(new JavaScriptSourceViewerConfiguration(textTools.getColorManager(), store, this, IJavaScriptPartitions.JAVA_PARTITIONING));
+//		}
+
+		
+		if (getSourceViewerConfiguration() instanceof JavaScriptSourceViewerConfiguration
+				|| getSourceViewerConfiguration() instanceof MyJavaScriptConfiguration) {
+		JavaScriptTextTools textTools= JavaScriptPlugin.getDefault().getJavaTextTools();	
+		setSourceViewerConfiguration(
+				new MyJavaScriptConfiguration(textTools.getColorManager(), 
+						store, this, IDocument.DEFAULT_CONTENT_TYPE));	
+		}
+		
+//		 IDocument document = getSourceViewer().getDocument();
+//		 document.addDocumentPartitioningListener(new IDocumentPartitioningListener() {
+//
+//			@Override
+//			public void documentPartitioningChanged(IDocument document) {
+//				// TODO Auto-generated method stub
+//				if(document.containsPositionCategory(IDocument.DEFAULT_CONTENT_TYPE))
+//				{
+//					
+//				}
+//			}
+//			 
+//		 });
+		 
+			if (getSourceViewer() instanceof JavaSourceViewer)
+				((JavaSourceViewer)getSourceViewer()).setPreferenceStore(store);
+	}
 }
+
 //
 //public class JavaScriptEditor extends CompilationUnitEditor {
 //	
