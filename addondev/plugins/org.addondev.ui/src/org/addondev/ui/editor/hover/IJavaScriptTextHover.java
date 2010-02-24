997d5b0c7f5246322a1404019a637d2699bfb180
s <s@localhost>
2010-02-25 06:59:18 +0900

    -

--- addondev/plugins/org.addondev.ui/src/org/addondev/ui/editor/hover/IJavaScriptTextHover.java
+++ addondev/plugins/org.addondev.ui/src/org/addondev/ui/editor/hover/IJavaScriptTextHover.java
@@ -1,8 +1,11 @@
 package org.addondev.ui.editor.hover;
 
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextHover;
 import org.eclipse.jface.text.ITextHoverExtension;
+import org.eclipse.jface.text.ITextViewer;
 
 public interface IJavaScriptTextHover extends ITextHover, ITextHoverExtension {
 	public void setContentType(String contentType);
+	//public void getHoverInfo(ITextViewer textViewer, IRegion hoverRegion, String contentType;
 }
